4474f3cf8825f8d6248d2ad26d55bcda3ac8a82d
AhmedAbogameel <gamilco81@gmail.com>
2021-10-24 19:06:28 +0200

    - Drawer

--- app/src/main/java/com/example/android_course/MainActivity.java
+++ app/src/main/java/com/example/android_course/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android_course;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 }
---
+++ app/src/main/java/com/example/android_course/SendFragment.java
@@ -0,0 +1,21 @@
+package com.example.android_course;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SendFragment extends Fragment {
+
+
+    public SendFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_send, container, false);
+    }
+}
